13d30ac51b8f22b47a21a6b8bcba1b9378e621fc
Johan Kämpe <kampe.johan@gmail.com>
2017-11-26 18:58:47 +0100

    Removed original comments

--- exercises/ex_005/main.c
+++ exercises/ex_005/main.c
@@ -18,87 +18,86 @@
 
 int main()
 {
-    int menu; //Variable for the number the user inputs
-    float num1, num2, result; //Float variables for the user input and output, used floats in case the user enters e.g. 14.7
+    int menu;
+    float num1, num2, result;
 
     printf("Enter a number from the list below\n\n");
 
-    printf("1. Addition\n"); //Calculator menu, user must enter a value from 1 - 4 for the program to work
+    printf("1. Addition\n");
     printf("2. Subtraction\n");
     printf("3. Multiplication\n");
     printf("4. Division\n\n");
 
-    printf("Enter number: "); //User input for the calculator menu
+    printf("Enter number: ");
     io_input_value_int(&menu);
 
     printf("\n");
 
-    switch(menu) //switch statement for menu
+    switch(menu)
     {
     case 1:
         printf("You entered Addition\n\n");
 
-        printf("Enter first number: "); //User input for first number
+        printf("Enter first number: ");
         io_input_value_float(&num1);
 
-        printf("Enter second number: "); //User input for second number
+        printf("Enter second number: ");
         io_input_value_float(&num2);
 
         printf("\n");
 
-        result = num1 + num2; //Addition calculation
+        result = num1 + num2;
 
-        printf("%.2f + %.2f = %.2f\n", num1, num2, result); //Addition output
+        printf("%.2f + %.2f = %.2f\n", num1, num2, result);
         break;
     case 2:
         printf("You entered Subtraction\n\n");
 
-        printf("Enter first number: "); //User input for first number
+        printf("Enter first number: ");
         io_input_value_float(&num1);
 
-        printf("Enter second number: "); //User input for second number
+        printf("Enter second number: ");
         io_input_value_float(&num2);
 
         printf("\n");
 
-        result = num1 - num2; //Subtraction calculation
+        result = num1 - num2;
 
-        printf("%.2f - %.2f = %.2f\n", num1, num2, result); //Subtraction output
+        printf("%.2f - %.2f = %.2f\n", num1, num2, result);
         break;
     case 3:
         printf("You entered Multiplication\n\n");
 
-        printf("Enter first number: "); //User input for first number
+        printf("Enter first number: ");
         io_input_value_float(&num1);
 
-        printf("Enter second number: "); //User input for second number
+        printf("Enter second number: ");
         io_input_value_float(&num2);
 
         printf("\n");
 
-        result = num1 * num2; //Multiplication calculation
+        result = num1 * num2;
 
-        printf("%.2f * %.2f = %.2f\n", num1, num2, result); //Multiplication Output
+        printf("%.2f * %.2f = %.2f\n", num1, num2, result);
         break;
     case 4:
         printf("You entered Division\n\n");
 
-        printf("Enter first number: "); //User input for first number
+        printf("Enter first number: ");
         io_input_value_float(&num1);
 
-        printf("Enter second number: "); //User input for first number
+        printf("Enter second number: ");
         io_input_value_float(&num2);
 
         printf("\n");
 
-        result = num1 / num2; //Division calculation
+        result = num1 / num2;
 
         printf("%.2f / %.2f = %.2f\n", num1, num2, result);
         break;
     default:
-        printf("Enter correct number e.g 1 - 4\n"); //Outputted if the user enters a value other than 1 - 4
+        printf("Enter correct number e.g 1 - 4\n");
         break;
     }
     return 0;
 }
-//End of code
