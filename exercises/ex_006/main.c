8e63c54673e912367a9a4a923f94c821056c9ca5
Johan Kämpe <kampe.johan@gmail.com>
2017-12-13 15:50:55 +0100

    Indentation corrections

--- exercises/ex_006/main.c
+++ exercises/ex_006/main.c
@@ -14,40 +14,47 @@
 
 int main(int argc, char const *argv[])
 {
-    char calc_op;
+    operation_id op_id;
     double val1, val2;
 
     printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &calc_op);
+    op_id = input_operation();
 
-    printf("Enter two operands: ");
-    scanf("%lf %lf", &val1, &val2);
-
-    switch(calc_op)
+    if(op_id == ERROR_OP_NOT_FOUND)
     {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",
-                val1, val2, operation_add(val1, val2));
-            break;
+        printf("Error! operator is not correct");
+    }
 
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",
-                val1, val2, operation_subtract(val1, val2));
-            break;
+    else
+    {
+        printf("Enter two operands: ");
+        scanf("%lf %lf", &val1, &val2);
 
-        case '*':
-            printf("%.1lf * %.1lf = %.1lf",
-                val1, val2, operation_multiply(val1, val2));
-            break;
+        switch(op_id)
+        {
+            case ADDITION:
+                printf("%.1lf + %.1lf = %.1lf",
+                    val1, val2, operation_add(val1, val2));
+                break;
 
-        case '/':
-            printf("%.1lf / %.1lf = %.1lf",
-                val1, val2, operation_divide(val1, val2));
-            break;
+            case SUBTRACTION:
+                printf("%.1lf - %.1lf = %.1lf",
+                    val1, val2, operation_subtract(val1, val2));
+                break;
 
-        default:
-            printf("Error! operator is not correct");
-            break;
+            case MULTIPLICATION:
+                printf("%.1lf * %.1lf = %.1lf",
+                    val1, val2, operation_multiply(val1, val2));
+                break;
+
+            case DIVISION:
+                printf("%.1lf / %.1lf = %.1lf",
+                    val1, val2, operation_divide(val1, val2));
+                break;
+
+            default:
+                break;
+        }
     }
 
     return 0;
