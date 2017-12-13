0db308443f478e7194f3a8855c85052f08bf1a85
Johan Kämpe <kampe.johan@gmail.com>
2017-12-13 14:19:25 +0100

    Refactoring: Formatting and variable names

--- exercises/ex_006/main.c
+++ exercises/ex_006/main.c
@@ -1,40 +1,48 @@
-// Program to create a simple calculator
-// Performs addition, subtraction, multiplication or division depending the input from user
+/**
+ * Calc operations main file
+ * TDD / Refactor exercise
+ * based on code from
+ * https://www.programiz.com/c-programming/c-switch-case-statement
+ *
+ * main.c
+ *
+ * By Johan Kampe
+ */
 
-# include <stdio.h>
+#include <stdio.h>
 
-int main() {
-
-    char operator;
-    double firstNumber,secondNumber;
+int main(int argc, char const *argv[])
+{
+    char calc_op;
+    double val1, val2;
 
     printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operator);
+    scanf("%c", &calc_op);
 
     printf("Enter two operands: ");
-    scanf("%lf %lf",&firstNumber, &secondNumber);
+    scanf("%lf %lf", &val1, &val2);
 
-    switch(operator)
+    switch(calc_op)
     {
         case '+':
-            printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, firstNumber+secondNumber);
+            printf("%.1lf + %.1lf = %.1lf", val1, val2, val1 + val2);
             break;
 
         case '-':
-            printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, firstNumber-secondNumber);
+            printf("%.1lf - %.1lf = %.1lf", val1, val2, val1 - val2);
             break;
 
         case '*':
-            printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, firstNumber*secondNumber);
+            printf("%.1lf * %.1lf = %.1lf", val1, val2, val1 * val2);
             break;
 
         case '/':
-            printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber/firstNumber);
+            printf("%.1lf / %.1lf = %.1lf", val1, val2, val1 / val2);
             break;
 
-        // operator is doesn't match any case constant (+, -, *, /)
         default:
             printf("Error! operator is not correct");
+            break;
     }
 
     return 0;
